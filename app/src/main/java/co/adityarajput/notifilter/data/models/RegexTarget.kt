46d9286dbc0e81f25da353ff6e24b344fedb71f3
bywilfried <bywilfried@hotmail.com>
2026-09-14 09:13:05 +0200

    Add channel and contextual notification targets

--- app/src/main/java/co/adityarajput/notifilter/data/models/RegexTarget.kt
+++ app/src/main/java/co/adityarajput/notifilter/data/models/RegexTarget.kt
@@ -10,5 +10,7 @@ enum class RegexTarget(val description: Int? = null) {
     CONTENT(R.string.content),
     OR(R.string.title_or_content),
     AND(R.string.title_and_content),
+    CONTEXT(R.string.contextual_data),
+    CHANNEL(R.string.notification_channel),
     EXPRESSION;
 }
