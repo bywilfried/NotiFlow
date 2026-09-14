46b48b47583d015c74fc8c33023dee04322b4f84
bywilfried <bywilfried@hotmail.com>
2026-09-14 09:49:34 +0200

    Fix notification search serialization imports

--- app/src/main/java/co/adityarajput/notifilter/data/models/NotificationSearch.kt
+++ app/src/main/java/co/adityarajput/notifilter/data/models/NotificationSearch.kt
@@ -2,6 +2,8 @@ package co.adityarajput.notifilter.data.models
 
 import co.adityarajput.notifilter.utils.containsMatchIn
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
 @Serializable
